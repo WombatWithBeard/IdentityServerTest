0bc69d1b821258a41ecc98fa1341fd7c06108d8a
Anton Borodin <boroda132@hotmail.com>
2020-03-04 10:28:00 +0300

    implicit flow

---
+++ JavaScriptClient/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JavaScriptClient.Views.Home
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
